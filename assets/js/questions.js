b1eb4da118a741220cff2f510b3e9f4fbb0d5e5b
DonnaIB <donnaimrie@gmail.com>
2020-12-29 10:32:04 +0000

    Questions.js added and some tweaks to nav colours

---
+++ assets/js/questions.js
@@ -0,0 +1,48 @@
+const questions = [
+
+	{
+	question: "How would you say 'risky' in Spanish?",
+	answers: [
+		{ ans1: 'arriesgado / ariesgada', correct: true },
+		{ ans2: 'transparente', correct: false },
+		{ ans3: 'riguroso / rigurosa', correct: false },
+		{ ans4: 'peligroso / peligrosa', correct: false }
+	]
+	},
+    {
+	question: "What does the verb 'despotricar' mean in English?",
+	answers: [
+		{ ans1: 'to dispute', correct: false },
+		{ ans2: 'to rant', correct: true },
+		{ ans3: 'to disappoint', correct: false },
+		{ ans4: 'to exercise', correct: false }
+	]
+	},	
+	{
+	question: "¿Qué significa la palabra 'cera' en inglés?",
+	answers: [
+		{ ans1: 'pig', correct: false },
+		{ ans2: 'zero', correct: false },
+		{ ans3: 'hair', correct: false },
+		{ ans4: 'wax', correct: true }
+	]
+	},
+	{
+	question: "What does the word 'atajo' mean in English?",
+	answers: [
+		{ ans1: 'traffic jam', correct: false },
+		{ ans2: 'attack', correct: false },
+		{ ans3: 'garlic', correct: false },
+		{ ans4: 'shortcut', correct: true }
+	]
+	},
+	{
+	question: "¿Cómo dirías 'sensible' en inglés?",
+	answers: [
+		{ ans1: 'sensible', correct: false },
+		{ ans2: 'sensitive', correct: true },
+		{ ans3: 'shy', correct: false },
+		{ ans4: 'simple', correct: false }
+	]
+	},
+]
