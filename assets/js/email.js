a4dbddc0174e160844ed614db787501df52fabf1
DonnaIB <donnaimrie@gmail.com>
2020-12-31 16:50:55 +0000

    JavaScript added for contact form and associated scripts added to contact page

---
+++ assets/js/email.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("service_1jer91p", "spanish_quiz",{
+        "fullName": contactForm.fullName.value,
+        "email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            document.getElementById("contact-form").reset();
+            alert("Thank you! We will be in touch shortly.")
+
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; 
+}
