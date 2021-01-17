15553a5c3a257b5918c867385a70ff9157da3d0b
DonnaIB <donnaimrie@gmail.com>
2021-01-17 14:22:32 +0000

    Scores html and js files updated to based on new addName page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,43 +1,20 @@
-const username = document.getElementById("scoreForm")
-const recentScoreBtn = document.getElementById("recent-score-btn")
 const mostRecentScore = localStorage.getItem("mostRecentScore")
+const viewScores = document.getElementById("viewScores")
 const nameList = document.getElementById("nameList")
-const newName = document.getElementById("newName")
+const playAgain = document.getElementById("playAgain")
 const recentScoresList = document.getElementById("recentScoresList")
 const recentScores = JSON.parse(localStorage.getItem('recentScores')) || []
-let userScore
+
 
 // Button Event Listener //
-recentScoreBtn.addEventListener('click', addName)
+viewScores.addEventListener('click', showScores)
 
-
-// Hide form and show List of names //
-function addName() {
+function showScores() {
     nameList.classList.remove('hide')
-    newName.classList.add('hide')   
-    scoreList()
-}
-
-// Set and retrieve names and scores from local storage  //
-function scoreList() {
-  userScore = {
-      name : username.name.value, 
-      score :  mostRecentScore
-  }
-
-    recentScores.push(userScore)
-
-    recentScores.sort((a,b) => {
-        return b.score - a.score
-    })
-
-    recentScores.splice(5)
-
-    localStorage.setItem('recentScores', JSON.stringify(recentScores))
-
+    viewScores.classList.add('hide')
+    playAgain.classList.add('hide')
     recentScoresList.innerHTML = 
     recentScores.map(recentScores => {
     return `<li class="recentScoreListItems">${recentScores.name}: ${recentScores.score}</li>`
 }).join("")
-
 }
